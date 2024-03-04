5b1e36997fcfed435b8b2fca3a8fe094292c4fa4
ynla3 <157063338+ynla3@users.noreply.github.com>
2024-03-04 20:24:00 +0000

    add portfolio.html file

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -43,7 +43,7 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
+     createPlatform(500, 300, 200, 20);
 
 
     
@@ -52,6 +52,9 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
+     createCollectable("steve", 500, 300, 20, 0.5);
+    createCollectable("grace", 500, 300);
+   createCollectable("max", 500, 300)
 
 
 
@@ -61,7 +64,7 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
+  createCannon("left", 600, 1000);
 
 
 
